43117da5b3b22b16b945a409209f00e80ee264b7
Boris <rbstapel@gmail.com>
2026-04-02 23:11:02 +0200

    Initial Commit

---
+++ PitchCircle/PitchCircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitchCircleApp.swift
+//  PitchCircle
+//
+//  Created by boris on 02/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct PitchCircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
