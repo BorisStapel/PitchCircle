4034e4dd62299781a9dd9280cc2e9a9bd4572825
Boris <rbstapel@gmail.com>
2026-04-05 21:53:02 +0200

    fix test

--- Core/Music/NoteConverter.swift
+++ Core/Music/NoteConverter.swift
@@ -1,11 +1,10 @@
 import Foundation
 
 struct NoteConverter: Sendable {
-    static let noteNames = ["C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B"]
-
     nonisolated static func convert(frequency: Float, referenceA4: Float = 440.0, confidence: Float = 0.0) -> PitchResult? {
         guard frequency > 0 else { return nil }
 
+        let noteNames = ["C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B"]
         let midi = 12 * log2(frequency / referenceA4) + 69
         let roundedMidi = Int(round(midi))
         let cents = Double((midi - Float(roundedMidi)) * 100)
